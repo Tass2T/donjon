3e4e86cf081b68be91c240de90ba9f925c62b5f9
tass2t <brunog90@gmail.com>
2023-08-28 17:30:09 +0200

    first graphic render

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
-class Game {}
+import * as PIXI from "pixi.js";
+
+class Game {
+  app: PIXI.Application;
+  constructor() {
+    this.app = new PIXI.Application({
+      width: 840,
+      height: 560,
+    });
+
+    this.init();
+  }
+
+  init() {
+    document.body.appendChild(this.app.view as HTMLCanvasElement);
+    const graphics = new PIXI.Graphics();
+    graphics.beginFill(0xffffff);
+    graphics.drawRect(0, 0, this.app.view.width, this.app.view.height);
+    this.app.stage.addChild(graphics);
+  }
+}
 
 const game = new Game();
