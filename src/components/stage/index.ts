d4c36051a7a0bb5f90729c9610707176e7186dec
tass2t <brunog90@gmail.com>
2023-09-05 17:40:25 +0200

    added ground (somehow)

--- src/components/stage/index.ts
+++ src/components/stage/index.ts
@@ -1,13 +1,32 @@
 import * as PIXI from "pixi.js";
+import constant from "../../constant.json";
 
 export default class Level {
   container: PIXI.Container;
+  groundContainer: PIXI.Container;
   constructor() {
     this.container = new PIXI.Container();
+    this.groundContainer = new PIXI.Container();
+    this.container.addChild(this.groundContainer);
 
-    const graphics = new PIXI.Graphics();
-    graphics.beginFill(0xff00ee);
-    graphics.drawRect(400, 200, 300, 200);
-    this.container.addChild(graphics);
+    this.setGround();
+  }
+
+  setGround() {
+    this.groundContainer.x = -10;
+    this.groundContainer.y = constant.HEIGHT - (constant.HEIGHT / 100) * 50;
+    const groundTexture = PIXI.Texture.from("ground/ground_test.png");
+    const nbOfLine = ((constant.HEIGHT / 100) * 50) / 30;
+
+    for (let j = 0; j < nbOfLine; j++) {
+      for (let i = 0; i <= 3000; i += 88) {
+        const groundSprite = PIXI.Sprite.from(groundTexture);
+        groundSprite.x = i - j * 11;
+        groundSprite.y = j * 26;
+        groundSprite.width = 100;
+        groundSprite.height = 100;
+        this.groundContainer.addChild(groundSprite);
+      }
+    }
   }
 }
