28968a39e0a73c65d95b949cced3f5de73fabe49
tass2t <brunog90@gmail.com>
2023-09-14 17:16:14 +0200

    add a rectangle for character lol

--- src/components/stage/index.ts
+++ src/components/stage/index.ts
@@ -31,5 +31,15 @@ export default class Level {
     }
   }
 
-  setCharacter() {}
+  setCharacter() {
+    const character = new PIXI.Graphics();
+    character.beginFill(0x00ff00);
+    character.drawRect(
+      50,
+      Math.floor(constant.HEIGHT - constant.HEIGHT / 3),
+      80,
+      150
+    );
+    this.container.addChild(character);
+  }
 }
