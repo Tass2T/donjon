22680f8e0ce59868c6a6cba2883e2bed33c5a7cf
tass2t <brunog90@gmail.com>
2023-09-20 13:55:49 +0200

    player can be moved

--- src/components/Characters/Player.ts
+++ src/components/Characters/Player.ts
@@ -5,10 +5,16 @@ export default class Player {
   container: PIXI.Container;
   spriteSheet: PIXI.Spritesheet | null;
   animatedSprite: PIXI.AnimatedSprite | null;
+  directionY: "UP" | "DOWN" | null;
+  directionX: "RIGHT" | "LEFT" | null;
+  moving: Boolean;
   constructor() {
     this.container = new PIXI.Container();
     this.spriteSheet = null;
     this.animatedSprite = null;
+    this.directionY = null;
+    this.directionX = "RIGHT";
+    this.moving = false;
   }
 
   async setCharacter() {
@@ -28,5 +34,48 @@ export default class Player {
     }
   }
 
-  update(inputs: Array<String>) {}
+  resolveInputs(inputs: Array<String>) {
+    if (!inputs.length) {
+      this.directionY = null;
+      this.directionX = null;
+      this.moving = false;
+    }
+    console.log(inputs);
+    const previousKeys: Array<String> = [];
+    inputs.forEach((item) => {
+      switch (item) {
+        case "KeyA":
+          if (!previousKeys.includes("KeyD")) this.directionX = "LEFT";
+          this.moving = true;
+          break;
+        case "KeyD":
+          if (!previousKeys.includes("KeyA")) this.directionX = "RIGHT";
+          this.moving = true;
+          break;
+        case "KeyW":
+          if (!previousKeys.includes("KeyS")) this.directionY = "UP";
+          this.moving = true;
+          break;
+        case "KeyS":
+          if (!previousKeys.includes("KeyW")) this.directionY = "DOWN";
+          this.moving = true;
+          break;
+        default:
+          break;
+      }
+      previousKeys.push(item);
+    });
+  }
+
+  moveSprite() {
+    if (this.directionY === "UP") this.animatedSprite.y -= 10;
+    if (this.directionY === "DOWN") this.animatedSprite.y += 10;
+    if (this.directionX === "LEFT") this.animatedSprite.x -= 10;
+    if (this.directionX === "RIGHT") this.animatedSprite.x += 10;
+  }
+
+  update(inputs: Array<String>) {
+    this.resolveInputs(inputs);
+    if (this.moving) this.moveSprite();
+  }
 }
