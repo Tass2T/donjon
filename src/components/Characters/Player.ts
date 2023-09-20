505777b978afdb512a64b54ce9a5d9b6a1312f3e
tass2t <brunog90@gmail.com>
2023-09-20 13:20:55 +0200

    animated sprite refacto

--- src/components/Characters/Player.ts
+++ src/components/Characters/Player.ts
@@ -4,25 +4,27 @@ import constant from "../../constant.js";
 export default class Player {
   container: PIXI.Container;
   spriteSheet: PIXI.Spritesheet | null;
+  animatedSprite: PIXI.AnimatedSprite | null;
   constructor() {
     this.container = new PIXI.Container();
     this.spriteSheet = null;
+    this.animatedSprite = null;
   }
 
   async setCharacter() {
     this.spriteSheet = await PIXI.Assets.load("characters/adventurer.json");
 
     if (this.spriteSheet) {
-      const animatedSprite = new PIXI.AnimatedSprite(
+      this.animatedSprite = new PIXI.AnimatedSprite(
         this.spriteSheet.animations["idle"]
       );
 
-      animatedSprite.animationSpeed = 0.1;
-      animatedSprite.anchor.set(0.5);
-      animatedSprite.x = 100;
-      animatedSprite.y = constant.HEIGHT / 1.4;
-      this.container.addChild(animatedSprite);
-      animatedSprite.play();
+      this.animatedSprite.animationSpeed = 0.1;
+      this.animatedSprite.anchor.set(0.5);
+      this.animatedSprite.x = 100;
+      this.animatedSprite.y = constant.HEIGHT / 1.4;
+      this.container.addChild(this.animatedSprite);
+      this.animatedSprite.play();
     }
   }
 
