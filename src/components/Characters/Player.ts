d5d843860a99342e9f34ac50e0b84dd7a2e1efa6
tass2t <brunog90@gmail.com>
2023-12-11 14:01:06 +0100

    clean

--- src/components/Characters/Player.ts
+++ src/components/Characters/Player.ts
@@ -57,8 +57,6 @@ export default class Player {
   }
 
   checkIfIsStillJumping(groundBody: Matter.Body) {
-    console.log(this.physicalBody.position.y, this.jumpInitialHeight);
-
     if (Matter.Collision.collides(this.physicalBody, groundBody)) {
       this.jumping = false;
     }
